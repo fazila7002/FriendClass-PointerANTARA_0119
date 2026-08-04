ec0c77b96d1a1cd9ad1e1ef35e20445d932cb1ed
fazila7002 <fazilatunnisa705@gmail.com>
2026-08-04 10:24:35 +0700

    deklarasi dan implementasi  friend function setNama

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -7,3 +7,17 @@ class mahasiswa
 private:
     string nama;
 };
+
+class mahasiswa
+{
+private:
+    string nama;
+public:
+    friend void setNama(mahasiswa &a, string);
+};
+
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
