14d313e2d961b2ccc2d74279e9a4707aa760a302
fazila7002 <fazilatunnisa705@gmail.com>
2026-08-04 10:12:57 +0700

    friend class dan method displayall untuk akses

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -28,3 +28,28 @@ void siswa::setId(int pId)
 {
     id = pId;
 }
+
+class orang
+{
+private:
+    string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa; 
+};
+
+class siswa
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayAll(orang &a);
+};
+
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
