8ad457ac7323cc65208d6c7df2ff139e42903d6e
fazila7002 <fazilatunnisa705@gmail.com>
2026-08-04 10:11:34 +0700

    class orang dan class siswa beserta atribut

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class siswa;
+class orang
+{
+private:
+    string nama;
+public:
+    void setNama(string pNama);
+};
+
+class siswa
+{
+private:
+    int id;
+public:
+    void setId(int pId);
+};
